9358870757ebc396898b9c5cfede775e1fd4717a
liulijin <liulijin@feinno.com>
2013-07-10 21:46:48 +0800

    first full code

--- hn/hn-analyse/src/hn_analyse/core.clj
+++ hn/hn-analyse/src/hn_analyse/core.clj
@@ -1,45 +1,34 @@
 (ns hn-analyse.core
   (:require [net.cgrand.enlive-html :as html]))
 
-
-(defn -main[]
-  (let [result (redis/zrange "hn-page" 0 -1)
-        all-news (map #(html/html-resourse (java.io.StringReader. (b64/decode (.getBytes %)))) result)]
-   (apply hn-result  (mqp #(hn-one-page (hn-headlines %) (hn-points %) (hn-comments %)) all-news))))
-
-(defn hn-headlines [h]
-  (let [t (html/select h [:td.title :a])]
-    map #({:name (html/text %) :link (-> % :attrs :href)}) t))
-
-(defn hn-points [h]
-  (map #(hash-map :points (.replaceAll (html/text %) "\\D" ""))  (html/select h [:td.subtext html/first-child])))
-
-(defn hn-comments [h]
-  (map #(hash-map :comments (.replaceAll (html/text %) "\\D" ""))  (html/select h [:td.subtext html/last-child])))
-
-(defn hn-one-page[hl hp hc]
-  (let [l (map-indexed (fn [x y] (merge y (nth hp x) (nth hc x))) hl)]
-    (sort-by :points > l)))
-
-(defn hn-result[ & pages]
-  (sort-by :points > (first (vals (group-by :name (concat pages))))))
-
-
 (defn parse-int [s]
   (let [ss (re-find #"[0-9]*" s)]
     (if (empty? ss) 0
       (Integer. ss))))
 
-(defn contents [res]
+(defn select-contents [res]
   (html/select res
                #{[:td.title :a]
                  [:td.subtext html/first-child]
                  [:td.subtext html/last-child]}))
 
-(defn parse [c]
-  (for [[title points comments] (partition 3 c)]
-    {:link (:href (:attrs title))
-     :title (html/text title)
-     :points (parse-int (html/text points))
-     :comments (parse-int (html/text comments))
-     :comments-link  (:href (:attrs comments))}))
+(defn gen-result [c]
+  (reduce #(assoc %1 (:comments-link %2) %2)  {}
+          (for [[title points comments] (partition 3 c)]
+            {:link (:href (:attrs title))
+             :title (html/text title)
+             :points (parse-int (html/text points))
+             :comments (parse-int (html/text comments))
+             :comments-link  (:href (:attrs comments))})))
+
+(defn files-to-results [d]
+  (map #(gen-result (select-contents %))
+       (map html/html-resource (load-files d))))
+
+(defn load-files [d]
+  (sort #(< (.lastModified %) (.lastModified %2))
+        (remove #(.isDirectory %)
+                (file-seq (clojure.java.io/file d)))))
+
+(defn -main [d]
+  (apply merge (files-to-results d)))
